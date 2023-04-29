db4c8a046b5f8996dd90863d84555e6a63c04157
Pikacnu <92923578+Pikacnu@users.noreply.github.com>
2023-04-29 17:13:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,18 @@
 const nextConfig = {
   experimental: {
     output: 'export',
+<<<<<<< Updated upstream
     distDir: ['images','appDir','output','distDir'],
+=======
+    images: true,
+    modern: true,
+    optimizeFonts: true,
+    optimizeImages: true,
+    optimizeCss: true,
+    scrollRestoration: true,
+    stats: true,
+    workerThreads: true,
+>>>>>>> Stashed changes
   },
 }
 
